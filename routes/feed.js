e3b79f4b08956312449f523d3183cf89b458725a
Joshua <110654709+josh-ini@users.noreply.github.com>
2024-12-09 22:34:55 +0100

    Update

--- routes/feed.js
+++ routes/feed.js
@@ -2,19 +2,19 @@ const express = require('express');
 const router = express.Router();
 
 const feedController = require('../controllers/feed');
-// const isAuth = require("../middleware/is-auth");
+const isAuth = require("../middleware/is-auth");
 
 
 // GET /feed/posts
-router.get("/posts",  feedController.getPosts);
+router.get("/posts",isAuth,  feedController.getPosts);
 
 // POST /feed/post
-router.post("/post", feedController.createPost);
+router.post("/post",isAuth ,feedController.createPost);
 // get single post
-router.get("/post/:postId", feedController.getPost);
+router.get("/post/:postId",isAuth, feedController.getPost);
 // update post
-router.put("/post/:postId", feedController.update)
+router.put("/post/:postId",isAuth, feedController.update)
 // delete post
-router.delete("post/:postId", feedController.delete);
+router.delete("post/:postId",isAuth, feedController.delete);
 
 module.exports = router;
